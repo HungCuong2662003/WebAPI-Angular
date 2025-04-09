70e6e21032064fd68d3a64ca0bc41c99b3e45ea4
HungCuong2662003 <cuong2661999@gmail.com>
2025-04-09 16:45:16 +0700

    update

--- WebAPI/WebAPI/obj/Release/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Release/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4defae0b5f56c6b3a2650d34659eb62ed95528")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8bc8ba68ce91fcd909ca09fb74b7564b9948c26")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
